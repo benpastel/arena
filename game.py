c4f26c932372bfbac6661b267aeac29a97e3839a
benji <benpastel@gmail.com>
2022-04-04 22:28:09 -0700

    print the player views too

--- game.py
+++ game.py
@@ -1,6 +1,13 @@
 
 
-from state import new_state, check_consistency, check_game_result, GameResult
+from state import (
+    new_state,
+    check_consistency,
+    check_game_result,
+    GameResult,
+    Player,
+    player_view
+)
 from display import print_state
 
 
@@ -11,4 +18,11 @@ if __name__ == '__main__':
     check_consistency(state)
     assert check_game_result(state) == GameResult.ONGOING
 
+    print("Private server state:")
     print_state(state)
+
+    print("\n\n\nNorth's view:")
+    print_state(player_view(state, Player.N))
+
+    print("\n\n\nSouth's view:")
+    print_state(player_view(state, Player.S))
